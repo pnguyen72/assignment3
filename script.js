05372d11463d0cf871822c755fbec5413edf9629
Felix <felix.fourcolor@gmail.com>
2024-10-05 18:01:34 -0700

    fix resolution threshold

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 video = document.getElementById("video")
 
-if (window.innerWidth >= 1600) {
+if (window.innerWidth > 1600) {
     video.play();
 }
 
 window.onresize = function () {
-    if (window.innerWidth < 1600) {
+    if (window.innerWidth <= 1600) {
         video.pause();
         video.currentTime = 0;
     } else {
