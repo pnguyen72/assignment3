e7954b9f25843dd534459d54353cc1c068c73eb2
Felix <felix.fourcolor@gmail.com>
2024-10-13 22:33:35 -0700

    format js

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-const body = document.querySelector("body")
-const jumpscare = document.getElementById("jumpscare")
-const gridContainer = document.getElementById("grid-container")
-const video = document.getElementById("video")
+const body = document.querySelector("body");
+const jumpscare = document.getElementById("jumpscare");
+const gridContainer = document.getElementById("grid-container");
+const video = document.getElementById("video");
 var videoEnded = false;
 
 function removeAllContent(backgroundColor = "initial") {
-    jumpscare.remove()
-    gridContainer.remove()
-    body.style.backgroundColor = backgroundColor
-    body.style.backgroundImage = "initial"
-    videoEnded = true;
+  jumpscare.remove();
+  gridContainer.remove();
+  body.style.backgroundColor = backgroundColor;
+  body.style.backgroundImage = "initial";
+  videoEnded = true;
 }
 
 if (navigator.getAutoplayPolicy("mediaelement") != "allowed")
-    removeAllContent()
-else
-    document.getElementById("autoplay-request").remove()
+  removeAllContent();
+else document.getElementById("autoplay-request").remove();
 
-
-if (window.innerWidth > 1600)
-    video.play()
+if (window.innerWidth > 1600) video.play();
 
 window.onresize = function () {
-    if (window.innerWidth <= 1600) {
-        video.pause()
-        video.currentTime = 0
-    } else if (!videoEnded) {
-        video.play()
-    }
-}
+  if (window.innerWidth <= 1600) {
+    video.pause();
+    video.currentTime = 0;
+  } else if (!videoEnded) {
+    video.play();
+  }
+};
 
 video.addEventListener("ended", (event) => {
-    removeAllContent(backgroundColor = "black")
+  removeAllContent((backgroundColor = "black"));
 });
